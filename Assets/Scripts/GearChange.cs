47dfe4fb34df41821c24275bc90fa863c67676e6
Name <91964534+a-usern4me@users.noreply.github.com>
2025-03-24 17:35:12 +0000

    Improved Magic Sequence, Idle Animations

--- Assets/Scripts/GearChange.cs
+++ Assets/Scripts/GearChange.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GearChange : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class GearChange : MonoBehaviour {
+    private Image gear;
+   
+    void Start(){
+        gear = this.GetComponent<Image>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+
+    void Update(){
+        if (Input.GetKey("d")){
+            gear.transform.Rotate(0.0f, 0.0f, 0.2f);
+            //gear.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+            //Debug.Log("Working");
+        }
+
+        if (Input.GetKey("a")){
+            gear.transform.Rotate(0.0f, 0.0f, -0.2f);
+        }
         
     }
 }
